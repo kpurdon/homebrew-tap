b62c3b1caccd9e1e4b749de757379d455043a886
Kyle Purdon <kylepurdon+scut@gmail.com>
2019-03-13 21:57:00 -0600

    Brew formula update for scut version v0.2.0

--- scut.rb
+++ scut.rb
@@ -2,9 +2,9 @@
 class Scut < Formula
   desc "Tail for RabbitMQ."
   homepage "https://github.com/kpurdon/scut"
-  url "https://github.com/kpurdon/scut/releases/download/v0.1.3/scut_0.1.3_Darwin_x86_64.tar.gz"
-  version "0.1.3"
-  sha256 "b08a240d78d6aad0b3667cb4610c5a13c1f7e48588edb60aad11dcde54fe1d2a"
+  url "https://github.com/kpurdon/scut/releases/download/v0.2.0/scut_0.2.0_Darwin_x86_64.tar.gz"
+  version "0.2.0"
+  sha256 "8ed11531fdd688e27191e3a27afb949d9fcb35c35f95e50f4b9139d1fc61206b"
 
   def install
     bin.install "scut"
